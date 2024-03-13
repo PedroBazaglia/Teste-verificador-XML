0e88278410c3776f8410b477aa5ff80ff4389d8c
= <=>
2024-03-13 17:26:34 -0300

    Tornando a progressbar operacional, tornando modelo como um arquivo fixo, adicionando scrollbar

--- Verificador.py
+++ Verificador.py
@@ -37,6 +37,9 @@ def comparar_estrutura_servidores(modelo, producao , output_text, progress_bar):
     tree_producao = ET.parse(producao)
     raiz_producao = tree_producao.getroot()
 
+    total_servidores_producao = len(list(raiz_producao.iter('servidor')))
+    progresso = 0
+
     for servidor_producao in raiz_producao.iter('servidor'):
         nome_servidor_producao = obter_nome_servidor(servidor_producao)
         estrutura_producao = obter_estrutura_servidor(servidor_producao)
@@ -47,6 +50,15 @@ def comparar_estrutura_servidores(modelo, producao , output_text, progress_bar):
         for atributo in estrutura_producao:
             if atributo not in estrutura_modelo:
                 output_text.insert(tk.END, f"Atributo '{atributo}' presente na produção está ausente no modelo. \n")
+        output_text.insert(tk.END, "-----------------------------------------------------\n")
+
+        # Atualizar o valor da barra de progresso
+        progresso += 100 / total_servidores_producao
+        progress_bar['value'] = progresso
+        root.update_idletasks()  # Atualizar a interface gráfica para refletir a mudança
+    
+    # Definir o valor da barra de progresso como 100 após a conclusão da comparação
+    progress_bar['value'] = 100
 
 def selecionar_arquivo():
     arquivo_xml = filedialog.askopenfilename(title="Selecione o arquivo XML", filetypes=[("Arquivos XML", "*.xml")])
@@ -54,7 +66,7 @@ def selecionar_arquivo():
 
 def comparar_arquivos(output_text, progress_bar):
     output_text.delete(1.0, tk.END)  # Limpar qualquer texto anterior
-    arquivo_xml_modelo = selecionar_arquivo()
+    arquivo_xml_modelo = "conf/modelo.xml"
     if not arquivo_xml_modelo:
         return
 
@@ -62,23 +74,12 @@ def comparar_arquivos(output_text, progress_bar):
     if not arquivo_xml_producao:
         return
 
+     # Definir o valor inicial da barra de progresso como 0
+    progress_bar['value'] = 0
+    root.update_idletasks()  # Atualizar a interface gráfica para refletir a mudança
+
     # Iniciar uma thread para executar a comparação, permitindo que a interface gráfica permaneça responsiva
     threading.Thread(target=comparar_estrutura_servidores, args=(arquivo_xml_modelo, arquivo_xml_producao, output_text, progress_bar)).start()
-
-
-def main():
-    # Seleciona o arquivo XML de modelo
-    arquivo_xml_modelo = selecionar_arquivo("Selecione o arquivo XML de Modelo")
-    if not arquivo_xml_modelo:
-        return
-
-    # Seleciona o arquivo XML de produção
-    arquivo_xml_producao = selecionar_arquivo("Selecione o arquivo XML de Produção")
-    if not arquivo_xml_producao:
-        return
-
-    # Realiza a comparação entre os servidores dos XMLs
-    comparar_estrutura_servidores(arquivo_xml_modelo, arquivo_xml_producao)
 
 # Criar a janela principal
 root = tk.Tk()
@@ -88,12 +89,24 @@ root.title("Comparador de Estrutura XML")
 btn_comparar = tk.Button(root, text="Comparar Arquivos XML", command=lambda: comparar_arquivos(output_text, progress_bar))
 btn_comparar.pack(pady=10)
 
+# Criar uma frame para conter a caixa de texto e a barra de rolagem
+frame_texto = tk.Frame(root)
+frame_texto.pack(fill="both", expand=True)
+
 # Widget de texto para exibir mensagens
-output_text = tk.Text(root, wrap="word")
-output_text.pack()
+output_text = tk.Text(frame_texto, width='100')
+output_text.pack(side="left", fill="both", expand=True)
+
+# Criar uma barra de rolagem vertical
+scrollbar = tk.Scrollbar(frame_texto, orient="vertical", command=output_text.yview)
+scrollbar.pack(side="right", fill="y")
+
+# Vincular a barra de rolagem ao widget de texto
+output_text.config(yscrollcommand=scrollbar.set)
 
 # Barra de progresso
-progress_bar = Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='indeterminate')
+progress_bar = Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='determinate')
+
 progress_bar.pack(pady=10)
 
 # Iniciar o loop da interface gráfica
